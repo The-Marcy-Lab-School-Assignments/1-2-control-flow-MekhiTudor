9f2deb8406bb153d6c41a604b4989e0f78243dbb
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-04-03 15:48:16 -0400

    question funTypes

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,11 +1,54 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought')
+  } else if (inches < 2) {
+    console.log('dry')
+  } else if (inches < 4) {
+    console.log('average')
+  } else if (inches < 6) {
+    console.log('rainy')
+  } else if (inches >= 6) {
+    console.log('flood')
+  }
 };
 
-const happyBirthdayPet = () => {
+const happyBirthdayPet = (breed, age) => {
+  if (breed === 'snake') {
+    console.log('Hiss hiss!')
+  } else if (breed === 'cat' && age < 5) {
+    console.log('Mew mew!')
+  } else if (breed === 'cat' && age >= 5) {
+    console.log('Meow meow!')
+  } else if (breed === 'dog' && age < 5) {
+    console.log('Arf arf!')
+  } else if (breed === 'dog' && age >= 5 && age < 10) {
+    console.log('Woof woof!')
+  } else if (breed === 'dog' && age >= 10) {
+    console.log('Boof!')
+  } else {
+    console.log('Happy birthday!')
+  }
 };
+const funTypes = (jsType) => {
+  if (typeof jsType === 'string') {
+    console.log('That\'s just some text')
+  } else if (Number.isNaN(jsType)) {
+    console.log("Well, now you're just showing off.")
+  } else if (typeof jsType === 'number') {
+    console.log('That\'s a good number.')
+  } else if (typeof jsType === 'boolean') {
+    console.log("To bool, or not to bool?")
+  } else if (typeof jsType === 'undefined') {
+    console.log("Nothing, but I didn't set that.")
+  } else if (jsType === null) {
+    console.log("Nothing, and I did set that.")
+  } else if (typeof jsType === 'object' && typeof jsType === 'object' && jsType.isArray) {
+    console.log("Anybody got the key?")
+  } else if (Array.isArray(jsType)) {
+    console.log("I order you to be indexed.")
+  }
+}
 
-const funTypes = () => {
-};
 
 const rounder = () => {
 };
