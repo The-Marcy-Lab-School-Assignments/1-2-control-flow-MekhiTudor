614e36496078538e10eb456153d2b22908e148b1
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-04-06 10:55:04 -0400

    bonus

--- src/bonus-switch.js
+++ src/bonus-switch.js
@@ -1,7 +1,51 @@
-const measureRainSwitch = () => {
+const measureRainSwitch = (inches) => {
+  switch (inches) {
+    case 0:
+      console.log("drought");
+      break;
+    case 1:
+      console.log("dry");
+      break;
+    case 2:
+      console.log("average");
+      break;
+
+    case 3:
+      console.log("average");
+      break;
+    case 4:
+      console.log("rainy");
+      break;
+    case 5:
+      console.log("rainy");
+      break;
+    case 6:
+      console.log("flood");
+      break;
+    case 7:
+      console.log("flood");
+      break;
+    case 10:
+      console.log("flood");
+      break;
+  }
 };
 
-const rounderSwitch = () => {
+console.log(measureRainSwitch(0))
+
+const rounderSwitch = (float, roundSetting) => {
+  switch (roundSetting) {
+    case "up":
+      return Math.ceil(float);
+      break;
+    case "down":
+      return Math.floor(float);
+      break;
+    case "honest":
+      return Math.round(float);
+      break;
+
+  }
 };
 
 module.exports = {
